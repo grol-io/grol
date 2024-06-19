ad92bbcbc9516cb99d37a9b6a7a4df242841b074
Laurent Demailly <ldemailly@gmail.com>
2024-06-19 12:33:08 -0700

    repl uses parser instead of just lexer

--- repl/repl.go
+++ repl/repl.go
@@ -6,10 +6,23 @@ import (
 	"io"
 
 	"github.com/ldemailly/gorpl/lexer"
-	"github.com/ldemailly/gorpl/token"
+	"github.com/ldemailly/gorpl/parser"
 )
 
 const PROMPT = "$ "
+
+func printParserErrors(out io.Writer, p *parser.Parser) bool {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return false
+	}
+
+	fmt.Fprintf(out, "parser has %d error(s)\n", len(errors))
+	for _, msg := range errors {
+		fmt.Fprintf(out, "parser error: %s\n", msg)
+	}
+	return true
+}
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -24,8 +37,11 @@ func Start(in io.Reader, out io.Writer) {
 		line := scanner.Text()
 		l := lexer.New(line)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%s %s\n", tok.Type, tok.Literal)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if printParserErrors(out, p) {
+			continue
 		}
+		fmt.Println(program.String())
 	}
 }
