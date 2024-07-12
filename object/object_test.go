031f2abb9c323f41b07cd2200b14ec25496d97e7
Laurent Demailly <ldemailly@gmail.com>
2024-07-12 13:35:48 -0700

    better if I don't forget to add files

---
+++ object/object_test.go
@@ -0,0 +1,31 @@
+package object_test
+
+import (
+	"testing"
+
+	"github.com/ldemailly/gorepl/object"
+)
+
+func TestStringMapKey(t *testing.T) {
+	hello1 := object.String{Value: "Hello World"}
+	hello2 := object.String{Value: "Hello World"}
+	diff1 := &object.String{Value: "My name is johnny"}
+	diff2 := &object.String{Value: "My name is johnny"}
+	if &hello1 == &hello2 {
+		t.Errorf("strings pointer somehow same, unexpected")
+	}
+
+	m := object.NewMap()
+	m.Pairs[hello1] = diff1
+
+	v, ok := m.Pairs[hello2]
+	if !ok {
+		t.Errorf("no value found for key %v", hello2)
+	}
+	if v != diff1 {
+		t.Errorf("value for key %v is %v, expected %v", hello2, v, diff1)
+	}
+	if object.Equals(diff1, diff2) != object.TRUE {
+		t.Errorf("values aren't equal, unexpected")
+	}
+}
