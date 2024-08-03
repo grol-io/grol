7ce629097f3c527d9b17e8354b0942b71f1fc1e2
Laurent Demailly <ldemailly@gmail.com>
2024-08-03 08:21:11 -0700

    Adding sin,cos,tan, ln (natural log) too

--- extensions/extension.go
+++ extensions/extension.go
@@ -22,6 +22,35 @@ func Init() {
 	if err != nil {
 		panic(err)
 	}
+	cmd = object.Extension{
+		Name:     "sin",
+		MinArgs:  1,
+		MaxArgs:  1,
+		ArgTypes: []object.Type{object.FLOAT},
+		Callback: sin,
+	}
+	err = object.CreateCommand(cmd)
+	if err != nil {
+		panic(err)
+	}
+	cmd.Name = "cos"
+	cmd.Callback = cos
+	err = object.CreateCommand(cmd)
+	if err != nil {
+		panic(err)
+	}
+	cmd.Name = "tan"
+	cmd.Callback = tan
+	err = object.CreateCommand(cmd)
+	if err != nil {
+		panic(err)
+	}
+	cmd.Name = "ln"
+	cmd.Callback = ln
+	err = object.CreateCommand(cmd)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func pow(args []object.Object) object.Object {
@@ -31,4 +60,17 @@ func pow(args []object.Object) object.Object {
 	exp := args[1].(object.Float).Value
 	result := math.Pow(base, exp)
 	return object.Float{Value: result}
+}
+
+func sin(args []object.Object) object.Object {
+	return object.Float{Value: math.Sin(args[0].(object.Float).Value)}
+}
+func cos(args []object.Object) object.Object {
+	return object.Float{Value: math.Cos(args[0].(object.Float).Value)}
+}
+func tan(args []object.Object) object.Object {
+	return object.Float{Value: math.Tan(args[0].(object.Float).Value)}
+}
+func ln(args []object.Object) object.Object {
+	return object.Float{Value: math.Log(args[0].(object.Float).Value)}
 }
