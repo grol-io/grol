3294151dbeb884df50b1287382c9fdd175f78a79
Laurent Demailly <ldemailly@gmail.com>
2024-08-03 08:44:19 -0700

    gofumt + adding sqrt,exp,asin,acos,atan

--- extensions/extension.go
+++ extensions/extension.go
@@ -51,6 +51,36 @@ func Init() {
 	if err != nil {
 		panic(err)
 	}
+	cmd.Name = "sqrt"
+	cmd.Callback = sqrt
+	err = object.CreateCommand(cmd)
+	if err != nil {
+		panic(err)
+	}
+	cmd.Name = "exp"
+	cmd.Callback = exp
+	err = object.CreateCommand(cmd)
+	if err != nil {
+		panic(err)
+	}
+	cmd.Name = "asin"
+	cmd.Callback = asin
+	err = object.CreateCommand(cmd)
+	if err != nil {
+		panic(err)
+	}
+	cmd.Name = "acos"
+	cmd.Callback = acos
+	err = object.CreateCommand(cmd)
+	if err != nil {
+		panic(err)
+	}
+	cmd.Name = "atan"
+	cmd.Callback = atan
+	err = object.CreateCommand(cmd)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func pow(args []object.Object) object.Object {
@@ -65,12 +95,35 @@ func pow(args []object.Object) object.Object {
 func sin(args []object.Object) object.Object {
 	return object.Float{Value: math.Sin(args[0].(object.Float).Value)}
 }
+
 func cos(args []object.Object) object.Object {
 	return object.Float{Value: math.Cos(args[0].(object.Float).Value)}
 }
+
 func tan(args []object.Object) object.Object {
 	return object.Float{Value: math.Tan(args[0].(object.Float).Value)}
 }
+
 func ln(args []object.Object) object.Object {
 	return object.Float{Value: math.Log(args[0].(object.Float).Value)}
+}
+
+func sqrt(args []object.Object) object.Object {
+	return object.Float{Value: math.Sqrt(args[0].(object.Float).Value)}
+}
+
+func exp(args []object.Object) object.Object {
+	return object.Float{Value: math.Exp(args[0].(object.Float).Value)}
+}
+
+func asin(args []object.Object) object.Object {
+	return object.Float{Value: math.Asin(args[0].(object.Float).Value)}
+}
+
+func acos(args []object.Object) object.Object {
+	return object.Float{Value: math.Acos(args[0].(object.Float).Value)}
+}
+
+func atan(args []object.Object) object.Object {
+	return object.Float{Value: math.Atan(args[0].(object.Float).Value)}
 }
