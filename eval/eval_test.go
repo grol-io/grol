81b97300d6b7db86d2701d3236a4df92952556c8
Laurent Demailly <ldemailly@gmail.com>
2024-08-25 18:34:12 -0700

    Add failing test for #179

--- eval/eval_test.go
+++ eval/eval_test.go
@@ -965,3 +965,18 @@ func TestCrashKeys(t *testing.T) {
 		t.Errorf("should not have errored: %v", err)
 	}
 }
+
+func TestParenInIf(t *testing.T) {
+	inp := `x=1; y=1; if (x)==y {42}`
+	s := eval.NewState()
+	res, err := eval.EvalString(s, inp, false)
+	if err != nil {
+		t.Fatalf("should not have errored: %v", err)
+	}
+	if res.Type() != object.INTEGER {
+		t.Fatalf("should have returned an integer, got %#v", res)
+	}
+	if res.Inspect() != "42" {
+		t.Errorf("wrong result, got %q", res.Inspect())
+	}
+}
