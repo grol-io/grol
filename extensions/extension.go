887cdbb918c0c043c470da2a6b3fed60de2d12ea
Laurent Demailly <ldemailly@gmail.com>
2024-09-02 09:41:56 -0700

    Adding time_parse including version without format that tries a bunch of possible formats

--- extensions/extension.go
+++ extensions/extension.go
@@ -172,6 +172,7 @@ func initInternal(c *Config) error {
 	createJSONAndEvalFunctions(c)
 	createStrFunctions()
 	createMisc()
+	createTimeFunctions()
 	return nil
 }
 
@@ -366,6 +367,9 @@ func createMisc() {
 		},
 	}
 	MustCreate(intFn)
+}
+
+func createTimeFunctions() {
 	MustCreate(object.Extension{
 		Name:    "time",
 		MinArgs: 0,
@@ -398,11 +402,11 @@ func createMisc() {
 		MinArgs:  1,
 		MaxArgs:  2,
 		ArgTypes: []object.Type{object.FLOAT, object.STRING},
-		Help:     "As returned by time(), in seconds since epoch, and optional TimeZone/location",
+		Help:     "Float as returned by time() and time_parse() in seconds since epoch, and optional TimeZone/location",
 		Callback: func(st any, _ string, args []object.Object) object.Object {
 			s := st.(*eval.State)
 			timeUsec := math.Round(args[0].(object.Float).Value * 1e6)
-			t := time.UnixMicro(int64(timeUsec))
+			t := time.UnixMicro(int64(timeUsec)).UTC()
 			if len(args) == 2 {
 				timeZone := args[1].(object.String).Value
 				location, err := time.LoadLocation(timeZone)
@@ -430,9 +434,70 @@ func createMisc() {
 			return m
 		},
 	})
+	MustCreate(object.Extension{
+		Name:     "time_parse",
+		MinArgs:  1,
+		MaxArgs:  2,
+		ArgTypes: []object.Type{object.STRING, object.STRING},
+		Help:     "Parse a time string with optional format, returns seconds since epoch",
+		Callback: func(st any, _ string, args []object.Object) object.Object {
+			s := st.(*eval.State)
+			inp := args[0].(object.String).Value
+			if len(args) == 1 {
+				t, err := TryParseTime(inp)
+				if err != nil {
+					return s.Error(err)
+				}
+				return object.Float{Value: float64(t.UnixMicro()) / 1e6}
+			}
+			format := args[1].(object.String).Value
+			t, err := time.Parse(format, inp)
+			if err != nil {
+				return s.Error(err)
+			}
+			return object.Float{Value: float64(t.UnixMicro()) / 1e6}
+		},
+	})
 }
 
 // --- implementation of the functions that aren't inlined in lambdas above.
+
+var parseFormats = []string{
+	time.RFC3339,
+	time.ANSIC,      //   = "Mon Jan _2 15:04:05 2006"
+	time.UnixDate,   //   = "Mon Jan _2 15:04:05 MST 2006"
+	time.RFC822,     //   = "02 Jan 06 15:04 MST"
+	time.RFC822Z,    //   = "02 Jan 06 15:04 -0700" // RFC822 with numeric zone
+	time.RFC850,     //   = "Monday, 02-Jan-06 15:04:05 MST"
+	time.RFC1123,    //   = "Mon, 02 Jan 2006 15:04:05 MST"
+	time.RFC1123Z,   //   = "Mon, 02 Jan 2006 15:04:05 -0700" // RFC1123 with numeric zone
+	time.RFC3339,    //   = "2006-01-02T15:04:05Z07:00"
+	time.Kitchen,    //   = "3:04PM"
+	time.Stamp,      //   = "Jan _2 15:04:05"
+	time.StampMilli, //   = "Jan _2 15:04:05.000"
+	time.DateTime,   //   = "2006-01-02 15:04:05"
+	time.DateOnly,   //   = "2006-01-02"
+	time.TimeOnly,   //   = "15:04:05"
+	"_2 Jan 2006",
+	"_2/1/2006", // try EU (ie sensible) style first.
+	"1/_2/2006",
+	"_2-Jan-2006",
+	"02/01/06",
+	"01/02/06",
+}
+
+func TryParseTime(input string) (time.Time, error) {
+	var t time.Time
+	var err error
+	for _, format := range parseFormats { // maybe consider grouping formats by length
+		t, err = time.Parse(format, input)
+		if err == nil {
+			log.Infof("Parsed %q with format %q to %v", input, format, t)
+			return t, nil
+		}
+	}
+	return time.Time{}, fmt.Errorf("unable to parse time: %v", input)
+}
 
 func pow(args []object.Object) object.Object {
 	// Arg len check already done through MinArgs and MaxArgs
