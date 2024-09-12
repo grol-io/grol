4e434918d00ba80359f4ace54e521578da743132
Laurent Demailly <ldemailly@gmail.com>
2024-09-11 18:23:17 -0700

    Extract and recompose functions: `first`, `rest` to extract, `defun` to create. (also moved time function to time. namespace) (#221)
    * Wip, extract and recompose functions
    
    * compact for this too
    
    * Add format() to pretty print a function
    
    * move the time functions in time.x instead of _; time()->time.now()
    
    * working version of defun
    
    * Add test and fix code to not use inspect which quotes strings
    
    * Add test for format() and also fix issue in args list, extended the test
    
    * fix rest()

--- extensions/extension.go
+++ extensions/extension.go
@@ -75,7 +75,7 @@ func initInternal(c *Config) error {
 
 	// for printf, we could expose current eval "Out", but instead let's use new variadic support and define
 	// printf as print(snprintf(format,..)) that way the memoization of output also works out of the box.
-	err := eval.AddEvalResult("printf", "func(format, ..){print(sprintf(format, ..))}")
+	err := eval.AddEvalResult("printf", "func(fmtstr, ..){print(sprintf(fmtstr, ..))}")
 	if err != nil {
 		return err
 	}
@@ -236,7 +236,56 @@ func createJSONAndEvalFunctions(c *Config) {
 	jsonFn.Name = "unjson"
 	jsonFn.Callback = evalFunc // unjson at the moment is just (like) eval hoping that json is map/array/...
 	MustCreate(jsonFn)
-
+	jsonFn.Name = "format"
+	jsonFn.Help = "returns a string, pretty printed function object"
+	jsonFn.ArgTypes = []object.Type{object.FUNC}
+	jsonFn.Callback = object.ShortCallback(func(args []object.Object) object.Object {
+		fn := args[0].(object.Function)
+		return object.String{Value: fn.Format()}
+	})
+	MustCreate(jsonFn)
+	jsonFn.Name = "defun"
+	jsonFn.Help = "defines a function from name (empty for lambda), arguments, statements (as returned by first/rest)"
+	jsonFn.ArgTypes = []object.Type{object.STRING, object.ARRAY, object.ARRAY}
+	jsonFn.MinArgs = 3
+	jsonFn.MaxArgs = 3
+	jsonFn.Callback = func(env any, _ string, args []object.Object) object.Object {
+		s := env.(*eval.State)
+		name := args[0].(object.String).Value
+		argArray := args[1].(object.Array)
+		stmtArray := args[2].(object.Array)
+		// brute force-ish
+		buf := strings.Builder{}
+		buf.WriteString("func ")
+		buf.WriteString(name) // will be a lamnda if empty.
+		buf.WriteString("(")
+		for i, a := range argArray.Elements() {
+			if a.Type() != object.STRING {
+				return s.Errorf("defun: unexpected type %s in arguments", a.Type())
+			}
+			if i > 0 {
+				buf.WriteString(",")
+			}
+			buf.WriteString(a.(object.String).Value)
+		}
+		buf.WriteString("){")
+		for _, stmt := range stmtArray.Elements() {
+			if stmt.Type() != object.STRING {
+				return s.Errorf("defun: unexpected type %s in statements", stmt.Type())
+			}
+			str := stmt.(object.String).Value
+			buf.WriteString(str)
+			buf.WriteString("\n")
+		}
+		strFn := buf.String()
+		log.LogVf("defun: %s", strFn)
+		o, err := eval.EvalString(s, strFn, false)
+		if err != nil {
+			return s.Error(err)
+		}
+		return o
+	}
+	MustCreate(jsonFn)
 	loadSaveFn := object.Extension{
 		MinArgs:  0, // empty only case - ie ".gr" save file.
 		MaxArgs:  1,
@@ -424,7 +473,7 @@ func createMisc() {
 
 func createTimeFunctions() {
 	MustCreate(object.Extension{
-		Name:    "time",
+		Name:    "time.now",
 		MinArgs: 0,
 		MaxArgs: 0,
 		Help:    "Date/time in seconds since epoch",
@@ -451,11 +500,11 @@ func createTimeFunctions() {
 		},
 	})
 	MustCreate(object.Extension{
-		Name:     "time_info",
+		Name:     "time.info",
 		MinArgs:  1,
 		MaxArgs:  2,
 		ArgTypes: []object.Type{object.FLOAT, object.STRING},
-		Help:     "Float as returned by time() and time_parse() in seconds since epoch, and optional TimeZone/location",
+		Help:     "Float as returned by time.now() and time.parse() in seconds since epoch, and optional TimeZone/location",
 		Callback: func(st any, _ string, args []object.Object) object.Object {
 			s := st.(*eval.State)
 			timeUsec := math.Round(args[0].(object.Float).Value * 1e6)
@@ -493,7 +542,7 @@ func createTimeFunctions() {
 		},
 	})
 	MustCreate(object.Extension{
-		Name:     "time_parse",
+		Name:     "time.parse",
 		MinArgs:  1,
 		MaxArgs:  2,
 		ArgTypes: []object.Type{object.STRING, object.STRING},
@@ -521,7 +570,7 @@ func createTimeFunctions() {
 // --- implementation of the functions that aren't inlined in lambdas above.
 
 var parseFormats = []string{
-	time.DateTime, //   = "2006-01-02 15:04:05" // first as that's what time_info().str returns (with usec).
+	time.DateTime, //   = "2006-01-02 15:04:05" // first as that's what time.info().str returns (with usec).
 	time.RFC3339,
 	time.ANSIC,    //   = "Mon Jan _2 15:04:05 2006"
 	time.UnixDate, //   = "Mon Jan _2 15:04:05 MST 2006"
