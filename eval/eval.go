ff500d67e9db1196622fdbdfdd372a10dd03523c
Geoffrey White <geoffpiercewhite@gmail.com>
2025-07-12 13:04:17 -0400

    and adding maintidx to nolint

--- eval/eval.go
+++ eval/eval.go
@@ -222,7 +222,7 @@ func (s *State) evalPostfixExpression(node *ast.PostfixExpression) object.Object
 // Doesn't unwrap return - return bubbles up.
 // Initially this was the one to use internally recursively, except for when evaluating a function
 // but now it's less clear because of the need to unwrap references too. TODO: fix/clarify.
-func (s *State) evalInternal(node any) object.Object { //nolint:funlen,gocognit,gocyclo // quite a lot of cases.
+func (s *State) evalInternal(node any) object.Object { //nolint:funlen,gocognit,gocyclo,maintidx // quite a lot of cases.
 	if s.Context != nil && s.Context.Err() != nil {
 		return s.Error(s.Context.Err())
 	}
